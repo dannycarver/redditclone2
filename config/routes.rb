01d3a4e3d4bfd61d0a47fcd072aabdb39f7cdf90
Danny Carver <sdannycarver@icloud.org>
2018-09-05 23:41:49 -0600

    worked on some routes and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'subs#index'
+ resources :subs do
+  resources :topics
+ end
+ get'/people',to:'people#index', as: 'all_the_people'
+ get'/person/:id',to:'people#show', as: 'the_one_person'
+ delete'/person/:id',to:'people#destroy', as: 'destroy_the_person'
 end
